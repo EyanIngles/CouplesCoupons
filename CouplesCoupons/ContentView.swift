e9af39229a6bf09e5885765038204773e48833a0
eyan ingles <eyaningles29@gmail.com>
2026-08-31 21:23:17 +1000

    Initial Commit

---
+++ CouplesCoupons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CouplesCoupons
+//
+//  Created by Eyan Ingles on 31/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CouplesCoupons/CouplesCouponsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouplesCouponsApp.swift
+//  CouplesCoupons
+//
+//  Created by Eyan Ingles on 31/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct CouplesCouponsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
